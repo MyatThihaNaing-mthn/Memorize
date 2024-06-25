8bf96182c6d3ae1600de6b14d172717debab22e0
Myat Thiha Naing <myatthihanaing.mthn@gmail.com>
2024-06-26 00:03:58 +0800

    Three different themes and shuffle cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,56 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🥏"]
+    var ballTheme = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏉", "🥏", "🎱", "🪀"]
+    var heartTheme = ["🩷", "❤️", "🧡", "💚", "🩵", "💙", "🖤","❤️‍🔥", "💓", "💔"]
+    var animalTheme = ["🐶", "🐱", "🐰", "🦊", "🐼", "🐸", "🙈", "🦁", "🦋", "🐝"]
+    @State var currentTheme:[String]
+    
+    init(){
+        ballTheme += ballTheme
+        heartTheme += heartTheme
+        animalTheme += animalTheme
+        _currentTheme = State(initialValue: animalTheme)
+    }
     @State var cardCount = 4
     var body: some View {
         VStack{
+            title
             ScrollView{
                 cards
             }
-            Spacer()
-            cardCountAdjusters
+            themeButtonSection
         }
-        .padding()
+        .padding(4)
+    }
+    
+    var title: some View {
+        Text("Memorize!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
     }
     
     var cards: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
-            ForEach(0..<cardCount, id: \.self){ index in
-                CardView(content: emojis[index])
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]){
+            ForEach(0..<currentTheme.count, id: \.self){ index in
+                CardView(content: currentTheme[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
     }
     
-    func cardCountAdjuster(by offset:Int, symbol:String) -> some View{
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount+offset < 1 || cardCount+offset > emojis.count)
+    func themeChanger(theme: [String], symbol:String, label:String) -> some View{
+        VStack{
+            Button(action: {
+                currentTheme = theme
+                currentTheme.shuffle()
+                print("executed", theme)
+            }, label: {
+                Image(systemName: symbol)
+            })
+            .disabled(currentTheme == theme)
+            Text(label).font(.callout)
+        }.foregroundColor(.blue)
+        
     }
     
-    var cardAdder: some View{
-        cardCountAdjuster(by: 1, symbol: "plus.square.on.square")
+    var animalThemeButton: some View{
+        themeChanger(theme: animalTheme, symbol: "pawprint.fill", label: "Animal")
     }
     
-    
-    var cardRemover: some View{
-        cardCountAdjuster(by: -1, symbol: "minus.square")
+    var heartThemeButton: some View{
+        themeChanger(theme: heartTheme, symbol: "heart.square.fill", label: "Heart")
     }
     
-    var cardCountAdjusters: some View{
+    var ballThemeButton: some View{
+        themeChanger(theme: ballTheme, symbol: "soccerball.circle.fill", label: "Ball")
+    }
+    
+    var themeButtonSection: some View{
         HStack{
-            cardAdder
             Spacer()
-            cardRemover
+            animalThemeButton
+            Spacer()
+            heartThemeButton
+            Spacer()
+            ballThemeButton
+            Spacer()
         }
         .imageScale(/*@START_MENU_TOKEN@*/.medium/*@END_MENU_TOKEN@*/)
         .font(.largeTitle)
     }
+    
 }
 
 struct CardView: View {
